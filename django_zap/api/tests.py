9c6c5d9ce7ba99fd377a8eaf80f493f6ede9864b
f3rn4nd0000 <42982873+f3rn4nd0000@users.noreply.github.com>
2023-02-15 19:37:05 -0300

    adiciona django ao projeto

--- django_zap/api/tests.py
+++ django_zap/api/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from api.zap import Zap
+import pytest
 
-# Create your tests here.
+def test_instantiate_class():
+    zap = Zap()
+
+def test_pass_number():
+    number = '9999-0000'
+    zap = Zap(number)
+    assert zap.number == number
+
+def test_pass_is_not_number():
+    number = 9
+    zap = Zap(number)
+    assert zap.number != number
+
+@pytest.mark.parametrize(
+    'number,converted_number',[
+        ('(75) 99988 - 7654', '5575999887654'),
+        ('(75) 90000 - 0000', '5575900000000'),
+        ('123(**@#(!12930!', '5512312930'),
+        ('+55(75)3655-0000', '557536550000'),
+        ('asdf(75)981230000ert', '5575981230000')
+    ])
+def test_convert(number, converted_number):
+    zap = Zap(number)
+    assert zap.converted_number == converted_number
+
+if __name__ == '__main__':
+    pytest.main(["-x", __file__])
--- django_zap/api/views.py
+++ django_zap/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
+
+def index(request):
+    if request.method == "GET":
+        return JsonResponse({"message": "Hello World"})
+
+# def check_number(request, number):
+#     if request.method == "GET":
+#         return J
