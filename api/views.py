e1c3987ff304afdc90f982e6cfbd874884251671
Ivan Neto <ivan.cr.neto@gmail.com>
2023-03-01 14:59:13 -0300

    suportando get

--- api/views.py
+++ api/views.py
@@ -7,15 +7,13 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def index(request):
     if request.method == "GET":
+        if 'url_input' in request.GET:
+            number = request.GET["url_input"]
+            zap = Zap(number)
+            return HttpResponseRedirect(f'https://wa.me/{zap.converted_number}')
+
         return render(request, "api/index.html")
-    # if request.method == "GET" and request.GET['q'] is not None:
-    #     number = request.GET['q']
-    #     zap = Zap(number)
-    #     return HttpResponseRedirect(f'https://wa.me/{zap.converted_number}')
     if request.method == "POST":
         number = request.POST["url_input"]
-        print(request.POST)
-        print(request)
-        print(number)
         zap = Zap(number)
         return HttpResponseRedirect(f'https://wa.me/{zap.converted_number}')
